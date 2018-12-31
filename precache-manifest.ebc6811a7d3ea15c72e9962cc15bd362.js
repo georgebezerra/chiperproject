f4517fe6c755eb1df12417f1db05c07fc5dd782e
George .S. Bezerra <george.contato@gmail.com>
2018-12-31 15:53:01 -0200

    Updates

---
+++ precache-manifest.ebc6811a7d3ea15c72e9962cc15bd362.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1a08fb9a2b8ab2fec5c8",
+    "url": "/static/js/main.1a08fb9a.chunk.js"
+  },
+  {
+    "revision": "ac9ccbd18fe43d9d9bf4",
+    "url": "/static/js/1.ac9ccbd1.chunk.js"
+  },
+  {
+    "revision": "1a08fb9a2b8ab2fec5c8",
+    "url": "/static/css/main.63da9dd4.chunk.css"
+  },
+  {
+    "revision": "eb72999bbaf97b9fa36824a77b006528",
+    "url": "/index.html"
+  }
+];
